7f130d63f0a39015c5e4d60eee8bbb7b0d82fb7d
DH <ghdejrghks@gmail.com>
2020-05-27 17:49:36 +0900

    gitTest01 Update

--- Step02_DataType/src/test/main/GitTest01.java
+++ Step02_DataType/src/test/main/GitTest01.java
@@ -5,6 +5,8 @@ public class GitTest01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("branch 테스트 중이야");
+		
+		System.out.println("branch 연습중이야");
 	}
 
 }
